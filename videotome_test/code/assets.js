4d2c36ca4326fe0323c6699451ca122ca528d2c7
Fafau06 <129664380+Fafau06@users.noreply.github.com>
2026-04-29 21:53:53 +0700

    Update assets.js

--- videotome_test/code/assets.js
+++ videotome_test/code/assets.js
@@ -1,43 +1,45 @@
-const IMGs = {
+const IMGs =
+{
+    FG_BLANK:"images/blank.png",
 
+    // bg assets
+    
     BG_BACKGROUND:"images/BG_TEXTURE1.png",
     BG_CLEAR:"images/BG_CLEAR.png",
     BG_TITLE:"images/BG_TITLE.png",
+    BG_WIDESCREEN1:"images/BG_WIDESCREEN1.png",
     BG_STATION:"images/STATION.png",
     BG_STATIONWIDE:"images/STATIONWIDE.png",
     BG_STARS:"images/BG_STARS.png",
     BG_GALLEY:"images/BG_GALLEY.png",
-    FG_BLANK:"images/blank.png",
+
+    // char sprites
+    
     FG_MIKU:"images/FG_MIKU.png",
     FG_DARKMIKU:"images/FG_DARKMIKU.png"
-
 };
 
-const VIDs = {
-
+const VIDs =
+{
     VID_TEST:"videos/test.webm",
     VID_TEST2:"videos/test2.webm",
     VID_THE:"videos/the.webm",
     VID_ROACH:"videos/roach.webm"
-
 };
-
 
 // use MUS_ for looping background music
 // max one music track playing at once
 // and SFX_ for oneshot sound effects that can play on top
 
-const MUSIC = {
-
+const MUSIC =
+{
     MUS_TEST:"music/pixelollie.mp3",
     SFX_TITLE:"music/SFX_TITLE.mp3",
     SFX_TEXT: "music/SFX_TEXT.mp3"
-
 };
 
-const SPEAKERS = {
-
+const SPEAKERS =
+{
   E: {name: "emily", colour: "#fea79a"},
   M: {name:"miku", colour:"#00ddc0"}
-
 };
