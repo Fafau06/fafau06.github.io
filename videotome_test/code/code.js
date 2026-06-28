153e62f71b9f902ab094765c74f06b1638a888a7
Fafau06 <129664380+Fafau06@users.noreply.github.com>
2026-06-28 23:35:42 +0700

    Update code.js

--- videotome_test/code/code.js
+++ videotome_test/code/code.js
@@ -981,7 +981,7 @@ function drawDialogue(str, speaker, speakerColour) {
     // wrap text with markers if someone speaking
     if (speaker !== 'narration')
     {
-        str = "\0" + DEFAULT.speechmarkLeft + str + DEFAULT.speechmarkRight;
+        str = "\n" + DEFAULT.speechmarkLeft + str + DEFAULT.speechmarkRight;
         spkr = speaker;
         spkrcolor = speakerColour;
     }
